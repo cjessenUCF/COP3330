c10d8eed976e8f7694353d6e98ec1ed5007d004c
Carrie Jessen <cjessen@cfl.rr.com>
2019-11-01 22:08:58 -0400

    Added error handling

--- jessen_PA4/jessen_p2/src/Application.java
+++ jessen_PA4/jessen_p2/src/Application.java
@@ -2,9 +2,13 @@ import java.io.IOException;
 
 public class Application {
 
-    public static void main(String[] args) throws IOException {
-	DuplicateCounter duplicateCounter = new DuplicateCounter();
-	duplicateCounter.count("problem2.txt");
-	duplicateCounter.write("unique_word_counts.txt");
-    }
+    public static void main(String[] args) {
+		try {
+			DuplicateCounter duplicateCounter = new DuplicateCounter();
+			duplicateCounter.count("problem2.txt");
+			duplicateCounter.write("unique_word_counts.txt");
+		} catch (IOException ex) {
+			System.out.println("Error occurred.  Check file");
+		}
+	}
 }
